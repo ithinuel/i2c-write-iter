0bd949e21b31048f55959958911615a07a3746ee
Wilfried Chauveau <wilfried.chauveau@ithinuel.me>
2024-01-08 05:44:29 +0000

    Fix missing parameter in sync write_iter_read & bump dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,23 @@
 #![no_std]
+#![allow(async_fn_in_trait)]
 
 pub use embedded_hal::i2c::{AddressMode, I2c};
 
 pub trait WriteIter<A: AddressMode>: I2c<A> {
-        /// Writes bytes obtained form the iterator `bytes`.
-        fn write_iter<'a, U>(&'a mut self, address: A, bytes: U) -> Result<(), Self::Error>
-        where
-            U: IntoIterator<Item = u8> + 'a;
+    /// Writes bytes obtained form the iterator `bytes`.
+    fn write_iter<'a, U>(&'a mut self, address: A, bytes: U) -> Result<(), Self::Error>
+    where
+        U: IntoIterator<Item = u8> + 'a;
 }
 
 pub trait WriteIterRead<A: AddressMode>: I2c<A> {
-        /// Writes bytes obtained form the iterator `bytes`, then read to fill `buffer`.
-        fn write_iter_read<'a, U>(&'a mut self, address: A, bytes: U) -> Result<(), Self::Error>
-        where
-            U: IntoIterator<Item = u8> + 'a;
+    /// Writes bytes obtained form the iterator `bytes`, then read to fill `buffer`.
+    fn write_iter_read<'a>(
+        &'a mut self,
+        address: A,
+        bytes: impl IntoIterator<Item = u8> + 'a,
+        buffer: &mut [u8],
+    ) -> Result<(), Self::Error>;
 }
 
 /// Exposes trait for async.await use cases. Requires the `async` feature.
@@ -26,12 +30,13 @@ pub mod non_blocking {
         async fn write_iter<'a, U>(&'a mut self, address: A, bytes: U) -> Result<(), Self::Error>
         where
             U: IntoIterator<Item = u8> + 'a;
-    }
 
-    pub trait WriteIterRead<A: AddressMode>: I2c<A> {
         /// Writes bytes obtained form the iterator `bytes`, then read to fill `buffer`.
-        async fn write_iter_read<'a, U>(&'a mut self, address: A, bytes: U, read: &mut [u8]) -> Result<(), Self::Error>
-        where
-            U: IntoIterator<Item = u8> + 'a;
+        async fn write_iter_read<'a>(
+            &'a mut self,
+            address: A,
+            bytes: impl IntoIterator<Item = u8> + 'a,
+            buffer: &mut [u8],
+        ) -> Result<(), Self::Error>;
     }
 }
